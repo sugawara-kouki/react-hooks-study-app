38a1512e39cd861ee8a58d6580ef2b0228e37423
sugawara-kouki <kou.720131@gmail.com>
2025-07-01 15:36:09 +0900

    feat: セットアップ完了 - CI/CD, ESLint, Prettierの設定を追加

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["build", ".react-router", "node_modules"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      // React Hooks rules
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+          allowExportNames: [
+            "links",
+            "meta",
+            "loader",
+            "action",
+            "headers",
+            "handle",
+          ],
+        },
+      ],
+    },
+  }
+);
